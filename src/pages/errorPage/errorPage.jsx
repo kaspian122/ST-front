f87300289e3675eb5f849759dcc348affed2073e
lizazav <liziktele2@gmail.com>
2020-04-10 14:10:59 +0700

    error404

--- src/pages/errorPage/errorPage.jsx
+++ src/pages/errorPage/errorPage.jsx
@@ -1,11 +1,27 @@
 import React, { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-
-function ErrorPage() {
+import { useDidMount } from '../../utils/hooks';
+import './errorPage.scss';
+function ErrorPage({ setTitle = () => {} }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const disciplines = useSelector(state => state.disciplines);
+  useDidMount(() => {
+    setTitle('');
+  });
+  const handleDisciplinesClick = useCallback(() => {
+    history.push(`/disciplines`);
+  }, [history]);
   return (
-    <div>
-      <span>ERROR404</span>
+    <div className="error-page">
+      <div className="error-page__text">
+        <span className="error-page__text_title">404</span>
+        <span>Страница не найдена</span>
+      </div>
+      <div className="error-page__button" onClick={handleDisciplinesClick}>
+        Вернуться на главную(?){' '}
+      </div>
     </div>
   );
 }
